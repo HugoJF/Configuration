c04430323b14201ab9c484154950f68f6847603e
unknown <hugo_jeller@hotmail.com>
2015-06-26 15:29:01 -0400

    Transformed Configuration static class into an object

--- src/configuration/Configuration.java
+++ src/configuration/Configuration.java
@@ -13,29 +13,29 @@ public class Configuration {
 	/**
 	 * HashMap containing set values for parameters and commands
 	 */
-	private static HashMap<String, String> config = new HashMap<String, String>();
+	private HashMap<String, String> config = new HashMap<String, String>();
 
 	/**
 	 * Array containing valid parameter and command names
 	 */
-	private static ArrayList<String> validParameters = new ArrayList<String>();
-	private static ArrayList<String> validCommands = new ArrayList<String>();
+	private ArrayList<String> validParameters = new ArrayList<String>();
+	private ArrayList<String> validCommands = new ArrayList<String>();
 
 	/**
 	 * Array containing needed parameters for execution
 	 */
-	private static ArrayList<String> neededParameters = new ArrayList<String>();
+	private ArrayList<String> neededParameters = new ArrayList<String>();
 
 	/**
 	 * Counters for valid parameters and commands
 	 */
-	public static int validParametersSet = 0;
-	public static int validCommandsSet = 0;
+	public int validParametersSet = 0;
+	public int validCommandsSet = 0;
 
 	/**
 	 * The log4j class
 	 */
-	private final static Logger LOGGER = Logger.getLogger(Configuration.class);
+	private final Logger LOGGER = Logger.getLogger(Configuration.class);
 
 	/**
 	 * Added parameter and it's value to the configuration HashMap
@@ -43,8 +43,8 @@ public class Configuration {
 	 * @param key - the parameter name
 	 * @param value - the parameter value
 	 */
-	public static void setConfiguration(String key, String value) {
-		Configuration.config.put(key, value);
+	public void setConfiguration(String key, String value) {
+		this.config.put(key, value);
 	}
 
 	/**
@@ -52,8 +52,8 @@ public class Configuration {
 	 * 
 	 * @param command - the command to be executed
 	 */
-	public static void setCommand(String command) {
-		Configuration.config.put(command, "true");
+	public void setCommand(String command) {
+		this.config.put(command, "true");
 	}
 
 	/**
@@ -62,8 +62,8 @@ public class Configuration {
 	 * @param key - command or parameter name
 	 * @return the command/parameter set value
 	 */
-	public static String getConfiguration(String key) {
-		return Configuration.config.get(key);
+	public String getConfiguration(String key) {
+		return this.config.get(key);
 	}
 
 	/**
@@ -72,8 +72,8 @@ public class Configuration {
 	 * @param command - the command name
 	 * @return true if command if set, false if it's not
 	 */
-	public static boolean isCommandSet(String command) {
-		String s = (String) Configuration.config.get(command);
+	public boolean isCommandSet(String command) {
+		String s = (String) this.config.get(command);
 		if (s == null) return false;
 		if (s.equalsIgnoreCase("true")) return true;
 		return false;
@@ -86,8 +86,8 @@ public class Configuration {
 	 * @param needed - if it's required for execution, verifyArgs() throw error
 	 *        if a needed parameter is not set
 	 */
-	public static void addNewValidParameter(String s, boolean needed) {
-		Configuration.validParameters.add(s);
+	public void addNewValidParameter(String s, boolean needed) {
+		this.validParameters.add(s);
 		if (needed) neededParameters.add(s);
 	}
 
@@ -96,8 +96,8 @@ public class Configuration {
 	 * 
 	 * @param s - the parameter name to be added
 	 */
-	public static void addNewValidParameter(String s) {
-		Configuration.validParameters.add(s);
+	public void addNewValidParameter(String s) {
+		this.validParameters.add(s);
 	}
 
 	/**
@@ -105,8 +105,8 @@ public class Configuration {
 	 * 
 	 * @param s - the command name to be added
 	 */
-	public static void addNewValidCommand(String s) {
-		Configuration.validCommands.add(s);
+	public void addNewValidCommand(String s) {
+		this.validCommands.add(s);
 	}
 
 	/**
@@ -115,18 +115,18 @@ public class Configuration {
 	 * @param path - a File object containing config file
 	 * @throws IOException any error caused by reading the file
 	 */
-	public static void readFromFile(File path) throws IOException {
+	public void readFromFile(File path) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(path.getAbsolutePath()));
 		String line = null;
 		String[] parts;
 		while ((line = reader.readLine()) != null) {
 			parts = line.split(" ");
-			if (Configuration.parameterExists(parts[0])) {
-				Configuration.setConfiguration(parts[0], parts[1]);
-				Configuration.validParametersSet++;
-			} else if (Configuration.isCommand(parts[0])) {
-				Configuration.setConfiguration(parts[0], "true");
-				Configuration.validCommandsSet++;
+			if (this.parameterExists(parts[0])) {
+				this.setConfiguration(parts[0], parts[1]);
+				this.validParametersSet++;
+			} else if (this.isCommand(parts[0])) {
+				this.setConfiguration(parts[0], "true");
+				this.validCommandsSet++;
 			}
 		}
 		reader.close();
@@ -135,16 +135,16 @@ public class Configuration {
 	/**
 	 * @param args - String array from main method
 	 */
-	public static void readFromRunArgs(String[] args) {
+	public void readFromRunArgs(String[] args) {
 		for (int i = 0; i < args.length; i++) {
-			if (Configuration.parameterExists(args[i])) {
+			if (this.parameterExists(args[i])) {
 				String key = args[i].substring(1);
 				String value = args[i + 1];
-				Configuration.setConfiguration(key, value);
-				Configuration.validParametersSet++;
-			} else if (Configuration.isCommand(args[i])) {
-				Configuration.setConfiguration(args[i].substring(2), "true");
-				Configuration.validCommandsSet++;
+				this.setConfiguration(key, value);
+				this.validParametersSet++;
+			} else if (this.isCommand(args[i])) {
+				this.setConfiguration(args[i].substring(2), "true");
+				this.validCommandsSet++;
 			}
 		}
 	}
@@ -153,9 +153,9 @@ public class Configuration {
 	 * Print entire configuration key set, containing all set command and/or
 	 * parameters
 	 */
-	public static void debugParameters() {
+	public void debugParameters() {
 		LOGGER.info("LOADED PARAMETERS DEBUGGING");
-		for (String key : Configuration.config.keySet()) {
+		for (String key : this.config.keySet()) {
 			LOGGER.info("[" + key + "] = [" + config.get(key) + "]");
 		}
 	}
@@ -166,8 +166,8 @@ public class Configuration {
 	 * @param p - the String to check
 	 * @return true if it's structured as command, false if not
 	 */
-	private static boolean isCommand(String p) {
-		for (String s : Configuration.validCommands) {
+	private boolean isCommand(String p) {
+		for (String s : this.validCommands) {
 			if (("--" + s).equals(p)) {
 				return true;
 			}
@@ -181,8 +181,8 @@ public class Configuration {
 	 * @param p - the parameter name to check existance
 	 * @return true if parameter exists, false if not
 	 */
-	private static boolean parameterExists(String p) {
-		for (String s : Configuration.validParameters) {
+	private boolean parameterExists(String p) {
+		for (String s : this.validParameters) {
 			if (("-" + s).equals(p)) return true;
 		}
 		return false;
@@ -193,9 +193,9 @@ public class Configuration {
 	 * 
 	 * @throws Exception a missing needed parameter
 	 */
-	public static void verifyArgs() throws Exception {
-		for (String s : Configuration.neededParameters) {
-			if (!Configuration.config.containsKey(s)) {
+	public void verifyArgs() throws Exception {
+		for (String s : this.neededParameters) {
+			if (!this.config.containsKey(s)) {
 				throw new Exception("Missing needed parameter: " + s);
 			}
 		}
@@ -207,7 +207,7 @@ public class Configuration {
 	 * 
 	 * @return config HashMap<String, String>
 	 */
-	public static HashMap<String, String> getConfig() {
+	public HashMap<String, String> getConfig() {
 		return config;
 	}
 }
